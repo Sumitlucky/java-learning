d5f775c73f5a5b906558d2d071614a9514ec6ba3
Sumit Kumar Singh <147710960+Sumitlucky@users.noreply.github.com>
2025-11-10 14:28:27 +0530

    Update SUMIT_CHAPTER_1_PRACTICE_SET.java

--- SUMIT_CHAPTER_1_PRACTICE_SET.java
+++ SUMIT_CHAPTER_1_PRACTICE_SET.java
@@ -5,23 +5,28 @@ import java.util.Scanner;
 public class SUMIT_CHAPTER_1_PRACTICE_SET {
     public static void main(String[] args) {
         // question - 1
-//        int a = 2;
-//        int b = 6;
-//        int c = 6;
-//        int d = a+b+c;
-//        System.out.println(d);
+       int a = 2;
+       int b = 6;
+       int c = 6;
+       int d = a+b+c;
+       System.out.println(d);
         // question - 2
-//        float subject1 = 50;
-//        float  subject2 = 80;
-//        float  subject3 = 89;
-//        float subject4 = 78;
-//        float  subject5 = 67;
-//        float  CGPA = (subject1+subject2+subject3+subject4+subject5)/50;
-//        System.out.println(CGPA);
+       float subject1 = 50;
+       float  subject2 = 80;
+       float  subject3 = 89;
+       float subject4 = 78;
+       float  subject5 = 67;
+       float  CGPA = (subject1+subject2+subject3+subject4+subject5)/50;
+       System.out.println(CGPA);
         //question - 3
         System.out.println("What is your name: ");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         System.out.println("Hello " + name + " have a good day");
+        //question - 5
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
     }
 }
+
