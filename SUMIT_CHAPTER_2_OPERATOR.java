d51b6038df201987554906774c3026c064cffff8
Sumit Kumar Singh <147710960+Sumitlucky@users.noreply.github.com>
2025-11-11 07:21:08 +0530

    Add files via upload

---
+++ SUMIT_CHAPTER_2_OPERATOR.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class SUMIT_CHAPTER_2_OPERATOR {
+    public static void main(String[] args) {
+    //Arithematic operator
+        int a = 4;
+    int b = 10 % a;
+    //Assignment Operator
+    int c = b;
+    //comparison Operator
+        System.out.println(76>9);
+    //logical operators
+    System.out.println(67>5 && 78>89);
+    //bitwise operator
+    System.out.println(5==8);
+
+
+    }
+}
