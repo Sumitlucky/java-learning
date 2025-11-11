08740663c9365795d926295d9de391a9ae98a409
Sumit Kumar Singh <147710960+Sumitlucky@users.noreply.github.com>
2025-11-11 07:36:28 +0530

    Add files via upload

---
+++ SUMIT_CHAPTER_2_OPERATOR_PRECEDENCE.java
@@ -0,0 +1,19 @@
+package com.company;
+public class SUMIT_CHAPTER_2_OPERATOR_PRECEDENCE {
+    public static void main(String[]args) {
+        int a = 6*5-23/4;
+        System.out.println(a);
+        //precedence and associativity
+        int  b = 56/4-34*2;
+        System.out.println(b);
+        // Quick Quiz
+        int x = 6;
+        int y = 3;
+        int k = x * y/2;
+
+        int 
+        System.out.println(k);
+
+
+    }
+}
