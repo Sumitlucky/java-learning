7f269e0978dc4a4fd06bbaae591837226044fa1b
Sumit Kumar Singh <147710960+Sumitlucky@users.noreply.github.com>
2025-11-16 10:24:52 +0530

    Add files via upload

---
+++ SUMIT_CHAPTER_4_CONDITIONALS_IF_ELSE.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class SUMIT_CHAPTER_4_CONDITIONALS_IF_ELSE {
+    public static void main(String[] args) {
+        // if else statement
+        int age = 14;
+        if(age>18) {
+            System.out.println("yes you can drive ");
+        }
+        else{
+            System.out.println("no you can not drive");
+        }
+
+
+    }
+}
